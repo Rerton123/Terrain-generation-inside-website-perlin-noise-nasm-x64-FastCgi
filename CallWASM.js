24205225dac85b3a2bb40fd4409a88264b3c92d7
Rerton123 <dovydasabraitis963@gmail.com>
2026-06-26 21:08:42 +0300

    Delete CallWASM.js

--- CallWASM.js
+++
@@ -1,9 +0,0 @@
-async function run() {
-    const bytes = await fetch("W.wasm").then(r => r.arrayBuffer());
-
-    const { instance } = await WebAssembly.instantiate(bytes);
-
-    console.log(instance.exports.answer());
-}
-
-run();
